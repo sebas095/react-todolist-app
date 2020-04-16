df3bee62141eb8c59afef3bd03e558caa4dbce19
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-04-15 21:13:22 -0500

    Updates

---
+++ precache-manifest.2011c1e60d43e1a8aa52104e397c3162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5cd78ba449e2cb13a2b737b4040e3a",
+    "url": "/react-todolist-app/index.html"
+  },
+  {
+    "revision": "79797bd12ce3f8589ee9",
+    "url": "/react-todolist-app/static/js/2.ce9df923.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist-app/static/js/2.ce9df923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7c0adc8e5548e563a2",
+    "url": "/react-todolist-app/static/js/main.b9899611.chunk.js"
+  },
+  {
+    "revision": "199907556ac3c86f2f02",
+    "url": "/react-todolist-app/static/js/runtime-main.a8cd67b3.js"
+  }
+]);
